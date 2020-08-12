8b234b6452d940c67f4cb7d70bc3e1bfa5df2212
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-08-12 19:41:04 +0530

    Game Completed!

--- game.c
+++ game.c
@@ -4,9 +4,28 @@
 
 int main()
 {   
-    int number;
+    int number, guess, nguesses=1;
     srand(time(0));
     number = rand() % 100 + 1;
-    printf("The number is %d", number);
+    // printf("The number is %d", number);
+    
+    do
+    {
+        printf("Guess the number between 1 to 100\n");
+        scanf("%d", &guess);
+
+        if(guess>number){
+            printf("HINT: Guess a lower number!! \n");
+        }
+        else if (guess<number)
+        {
+            printf("HINT: Guess a higher number!! \n");
+        }
+        else{
+            printf("Correct!! You guessed it in %d attempts.\n", nguesses);
+        }
+        nguesses++;
+    } while(guess!=number);
+    
     return 0;
 }
